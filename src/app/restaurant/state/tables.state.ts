ece900146eda752617decf47e1025d3e99216944
Fanis Prodromou <prodromouf@gmail.com>
2023-09-30 17:15:49 +0300

    feat: prepare solution 5

--- src/app/restaurant/state/tables.state.ts
+++ src/app/restaurant/state/tables.state.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Action, NgxsOnInit, State, StateContext } from '@ngxs/store';
 
+import { insertItem, patch, removeItem } from '@ngxs/store/operators';
 import { tap } from 'rxjs/operators';
-import { OrdersMap, Table } from '../models';
+import { Order, OrdersMap, Table } from '../models';
 import { TablesApiService } from '../services/tables-api.service';
 import { AddTableChoice, CancelReservation, LoadTables, RemoveTableChoice, ReserveTable } from './actions';
 export interface TablesStateModel {
@@ -34,67 +35,49 @@ export class TablesState implements NgxsOnInit {
   protected reserveTable(ctx: StateContext<TablesStateModel>, action: ReserveTable): void {
     const { tableName } = action;
 
-    ctx.patchState({
-      orders: {
-        ...ctx.getState().orders,
-        [tableName]: { tableName, choices: [] },
-      },
-    });
+    ctx.setState(
+      patch({
+        orders: patch({
+          [tableName]: { tableName, choices: [] },
+        }),
+      })
+    );
   }
 
   @Action(CancelReservation)
   protected cancelReservation(ctx: StateContext<TablesStateModel>, action: CancelReservation): void {
     const { tableName } = action;
 
-    ctx.patchState({
-      orders: {
-        ...ctx.getState().orders,
-        [tableName]: null,
-      },
-    });
+    ctx.setState(
+      patch({
+        orders: patch({
+          [tableName]: undefined,
+        }),
+      })
+    );
   }
 
   @Action(AddTableChoice)
   protected addTableChoice(ctx: StateContext<TablesStateModel>, action: AddTableChoice): void {
     const { tableName, choice } = action;
-    // ctx.setState(
-    //   patch<TablesStateModel>({
-    //     orders: patch<OrdersMap>({
-    //       [tableName]: patch<Order>({ choices: insertItem(choice) }),
-    //     }),
-    //   })
-    // );
-
-    ctx.patchState({
-      orders: {
-        ...ctx.getState().orders,
-        [tableName]: {
-          ...ctx.getState().orders[tableName],
-          choices: [...ctx.getState().orders[tableName].choices, choice],
-        },
-      },
-    });
+    ctx.setState(
+      patch<TablesStateModel>({
+        orders: patch<OrdersMap>({
+          [tableName]: patch<Order>({ choices: insertItem(choice) }),
+        }),
+      })
+    );
   }
 
   @Action(RemoveTableChoice)
   protected removeTableChoice(ctx: StateContext<TablesStateModel>, action: RemoveTableChoice): void {
     const { tableName, choice } = action;
-    // ctx.setState(
-    //   patch<TablesStateModel>({
-    //     orders: patch<OrdersMap>({
-    //       [tableName]: patch<Order>({ choices: removeItem(item => item === choice) }),
-    //     }),
-    //   })
-    // );
-
-    ctx.patchState({
-      orders: {
-        ...ctx.getState().orders,
-        [tableName]: {
-          ...ctx.getState().orders[tableName],
-          choices: ctx.getState().orders[tableName].choices.filter(item => item !== choice),
-        },
-      },
-    });
+    ctx.setState(
+      patch<TablesStateModel>({
+        orders: patch<OrdersMap>({
+          [tableName]: patch<Order>({ choices: removeItem(item => item === choice) }),
+        }),
+      })
+    );
   }
 }
